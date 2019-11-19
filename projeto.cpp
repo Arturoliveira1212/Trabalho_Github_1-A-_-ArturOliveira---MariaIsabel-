59a32dfca36993349eafeb856d4efa0260017550
Arturoliveira1212 <arturalvesdeoliveira28@gmaill.com>
2019-11-19 15:08:54 -0300

    implementei a primeira função

---
+++ projeto.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+
+/*
+	Conta todas as letras 'letra' contidas em 'texto'
+*/
+int contaLetras(char letra, string texto)
+{
+    int i=0;
+    int j=0;
+    while(i<texto.length())
+    {
+        if(letra==texto[i])
+        {
+            j++;
+        }
+        i++;
+    }
+}
+
+/*
+	Substitui todas as ocorrencias de 'letra1' por 'letra2' contidas em 'texto'
+*/
+void substituirLetra(char letra1,char letra2, string texto)
+{
+}
+
+/*
+	Conta todas as ocorrencias de 'palavra' contidas em 'texto'
+*/
+int contaPalavras(string palavra, string texto)
+{
+}
+
+/*
+	Substitui todas as ocorrencias de 'palavra1' por 'palavra2' em 'texto'
+*/
+void substituirPalavras(string palavra1, string palavra2, string texto)
+{
+}
+
+int main(){
+
+	cout << "Entre com o texto para fazer o teste:";
+
+
+
+
+	return 0;
+}
